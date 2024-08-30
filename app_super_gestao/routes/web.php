778ab0634fe1a430c0d6387462d798947f0927f2
Iagooalmeida <iago732@gmail.com>
2024-08-30 15:43:34 -0300

    feat: Adicionar links de navegação nas views
    Adiciona links de navegação nas views de contato, principal e sobre nós para facilitar a navegação entre as páginas do aplicativo Super Gestão com Laravel.
    
    Co-authored-by: [Iago]

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -3,18 +3,50 @@
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', 'App\Http\Controllers\PrincipalControler@principal');
+Route::get('/', 'App\Http\Controllers\PrincipalControler@principal')->name('site.index');
 
-Route::get('/sobre-nos', 'App\Http\Controllers\SobreNosControler@sobreNos');
+Route::get('/sobre-nos', 'App\Http\Controllers\SobreNosControler@sobreNos')->name('site.sobrenos');
 
-Route::get('/contato', 'App\Http\Controllers\ContatoControler@contato');
+Route::get('/contato', 'App\Http\Controllers\ContatoControler@contato')->name('site.contato');
+
+Route::get('/login', function() {
+    return 'Login';
+})->name('site.login');
+
+Route::prefix('/app')->group(function() {
+    Route::get('/clientes', function() {
+        return 'Clientes';
+    })->name('app.clientes');
+
+    Route::get('/fornecedores', function() {
+        return 'Fornecedores';
+    })->name('app.fornecedores');
+
+    Route::get('/produtos', function() {
+        return 'Produtos';
+    })->name('app.produtos');
+});
+
+Route::get('/rota1', function() {
+    echo 'Rota 1';
+})->name('site.rota1');
+
+Route::get('/rota2', function() {
+    return redirect()->route('site.rota1');
+})->name('site.rota2');
+
+Route::fallback(function() {
+    echo 'A rota acessada não existe. <a href="'.route('site.index').'">Clique aqui</a> para ir para a página inicial';
+});
+
+//Route::redirect('/rota2', '/rota1');
 
 
-Route::get('/contato/{nome}/{categoria_Id}',
-function(string $nome, int $categoria = 1) {
-    echo "Estamos aqui: $nome - $categoria";
-})->where('categoria', '[0-9]+')
-->where('nome', '[A-Za-z]+');
+// Route::get('/contato/{nome}/{categoria_Id}',
+// function(string $nome, int $categoria = 1) {
+//     echo "Estamos aqui: $nome - $categoria";
+// })->where('categoria', '[0-9]+')
+// ->where('nome', '[A-Za-z]+');
 
 
 /*
