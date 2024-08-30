b0f9544d9c0810e45bb528cb2b2c43ae9f714394
Iagooalmeida <iago732@gmail.com>
2024-08-30 14:34:06 -0300

    feat: Adicionar rota de contato com parâmetros opcionais
    A rota de contato foi atualizada para permitir parâmetros opcionais, como categoria, assunto e mensagem. Isso permite uma maior flexibilidade ao enviar informações através do formulário de contato.
    
    Essa alteração foi feita para melhorar a funcionalidade do aplicativo Super Gestão com Laravel.
    
    Co-authored-by: [Iago]

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -9,7 +9,21 @@ Route::get('/sobre-nos', 'App\Http\Controllers\SobreNosControler@sobreNos');
 
 Route::get('/contato', 'App\Http\Controllers\ContatoControler@contato');
 
+
+Route::get('/contato/{nome}/{categoria_Id}',
+function(string $nome, int $categoria = 1) {
+    echo "Estamos aqui: $nome - $categoria";
+})->where('categoria', '[0-9]+')
+->where('nome', '[A-Za-z]+');
+
+
 /*
+
+Route::get('/contato/{nome}/{categoria?}/{assunto?}/{mensagem?}',
+function(string $nome, string $categoria = 'Informação', string $assunto = 'Contato', string $mensagem = 'Mensagem não enviada') {
+    echo "Estamos aqui: $nome - $categoria - $assunto - $mensagem";
+});
+
 // Route::get('/', function () {
 //     //return view('welcome');
 //     return 'Olá, seja bem-vindo ao curso!';
