776a159ff0d602d7cf1951889b0fac80abae8fa0
Iagooalmeida <iago732@gmail.com>
2025-01-17 00:11:25 -0300

    feat: Adicionar configuração de tabelas e atributos nos modelos Fornecedor e SiteContato

--- app_super_gestao/app/Models/Sitecontato.php
+++ app_super_gestao/app/Models/Sitecontato.php
@@ -5,7 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Sitecontato extends Model
+class SiteContato extends Model
 {
     use HasFactory;
+    protected $table = 'sitecontatos';
 }
